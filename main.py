e7f01ba733d8801a6e5ee69894736ade611cf8de
brianSalk <brian.salkas@gmail.com>
2025-04-07 02:53:08 +0300

    Start project

---
+++ main.py
@@ -0,0 +1,10 @@
+# add next letter
+# check all words for match
+# if match, add to set
+# once all words <= 10 letters long are found, connect them by first and last letter
+class Letter:
+    def __main__(self, char, side):
+        self.char = letter
+        self.side = side
+
+sides = [[],[],[],[]]
